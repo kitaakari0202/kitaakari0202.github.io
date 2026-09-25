edecb7663fe6ff22913f1bffb15ce5ac8f06d7f5
kitaakari0202 <syuu9533@gmail.com>
2026-09-25 19:33:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const galleryElement = document.getElementById("gallery");
 
 const images = [];
 
-for (let i = 16; i >= 1; i--) {
+for (let i = 17; i >= 1; i--) {
     const number = String(i).padStart(2, "0");
     images.push(number + ".png");
 }
